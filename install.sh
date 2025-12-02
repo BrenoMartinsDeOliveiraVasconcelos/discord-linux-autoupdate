b09b7ef25315df33532ae33a19b5fbf060691fa6
Breno Martins <breno05martins@gmail.com>
2025-12-02 14:02:06 -0300

    Enhanced installer script

--- install.sh
+++ install.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+# CHeck if running as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
 script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$script_path"
 install_dir="/usr/local/share/discord-linux-autoupdate"
 
 if command -v apt-get &> /dev/null; then
     echo "Dependency installation for Debian-based distributions has started."
-    sudo apt-get update
+    apt-get update
     userwantsgui='n'
     read -p "[OPTIONAL] Do you want to install the GUI dependencies? (y/n): " userwantsgui
 
     if [[ "$userwantsgui" == "y" || "$userwantsgui" == "Y" ]]; then
-        sudo apt-get install -y python3-tk
+        apt-get install -y python3-tk
     fi
 
-    sudo apt-get install -y python3-venv
+    apt-get install -y python3-venv
 else
     echo "This software only supports Debian-based distributions due Discord linux package format."
     exit 1
@@ -22,22 +28,22 @@ fi
 
 
 echo "Creating necessary directories..."
-sudo mkdir -p $install_dir
+mkdir -p $install_dir
 
 echo "Copying files..."
-sudo cp -r ./* $install_dir
+cp -r ./* $install_dir
 
 echo "Creating necessary files..."
-sudo touch "$install_dir/last_saved.json"
-echo "{}" | sudo tee "$install_dir/last_saved.json" > /dev/null
+touch "$install_dir/last_saved.json"
+echo "{}" | tee "$install_dir/last_saved.json" > /dev/null
 
 echo "Creating launchers..."
-sudo cp -r cli_run.sh /usr/local/bin/discord-updater
-sudo cp -r gui_run.sh /usr/local/bin/discord-updater-gui
+cp -r cli_run.sh /usr/local/bin/discord-updater
+cp -r gui_run.sh /usr/local/bin/discord-updater-gui
 
 echo "Installing virtual environment and dependencies..."
 cd $install_dir
-sudo python3 -m venv venv
+python3 -m venv venv
 
 source ./venv/bin/activate
 pip install --upgrade pip
@@ -45,14 +51,14 @@ pip install -r requirements.txt
 deactivate
 
 echo "Chowning files to root"
-sudo chown -R root:root "$install_dir"
-sudo chown root:root "/usr/local/bin/discord-updater"
-sudo chown root:root "/usr/local/bin/discord-updater-gui"
+chown -R root:root "$install_dir"
+chown root:root "/usr/local/bin/discord-updater"
+chown root:root "/usr/local/bin/discord-updater-gui"
 
 echo "Setting permissions..."
-sudo chmod a+x /usr/local/bin/discord-updater
-sudo chmod a+x /usr/local/bin/discord-updater-gui
-sudo chmod -R a+rX "$install_dir"
-sudo chmod a+rX "$install_dir/run_discord.sh"
-sudo chmod a+rw "$install_dir/config.json"
-sudo chmod a+rw "$install_dir/last_saved.json"
+chmod a+x /usr/local/bin/discord-updater
+chmod a+x /usr/local/bin/discord-updater-gui
+chmod -R a+rX "$install_dir"
+chmod a+rX "$install_dir/run_discord.sh"
+chmod a+rw "$install_dir/config.json"
+chmod a+rw "$install_dir/last_saved.json"
