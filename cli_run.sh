b80e4ac6116069ec561e39f70219638e36d5624b
Breno Martins <breno05martins@gmail.com>
2025-12-02 11:37:02 -0300

    Installattion sscripts created

---
+++ cli_run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 /usr/local/share/discord-linux-autoupdate/main.py cli
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$script_path"
+install_dir="/usr/local/share/discord-linux-autoupdate"
+
+if command -v apt-get &> /dev/null; then
+    echo "Dependency installation for Debian-based distributions has started."
+    sudo apt-get update
+    userwantsgui='n'
+    read -p "[OPTIONAL] Do you want to install the GUI dependencies? (y/n): " userwantsgui
+
+    if [[ "$userwantsgui" == "y" || "$userwantsgui" == "Y" ]]; then
+        sudo apt-get install -y python3-tk
+    fi
+else
+    echo "This software only supports Debian-based distributions due Discord linux package format."
+    exit 1
+fi
+
+
+echo "Creating necessary directories..."
+sudo mkdir -p $install_dir
+
+echo "Copying files..."
+sudo cp -r ./* $install_dir
+
+echo "Creating CLI and GUI launchers..."
+sudo cp -r cli_run.sh /usr/local/bin/discord-updater
+sudo cp -r gui_run.sh /usr/local/bin/discord-updater-gui
+
+echo "Chowning files to root"
+sudo chown -R root:root "$install_dir"
+sudo chown root:root "/usr/local/bin/discord-updater"
+sudo chown root:root "/usr/local/bin/discord-updater-gui"
+
+echo "Setting executable permissions..."
+sudo chmod a+x /usr/local/bin/discord-updater
+sudo chmod a+x /usr/local/bin/discord-updater-gui
+sudo chmod -R a+rwX "$install_dir"
