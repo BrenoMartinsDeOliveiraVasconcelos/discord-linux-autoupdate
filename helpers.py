c1b462c8c5a7d6fb8b24f1bb7acf844a2539892b
Breno Martins <breno05martins@gmail.com>
2025-12-02 09:33:34 -0300

    Download deb files successfully

--- helpers.py
+++ helpers.py
@@ -3,7 +3,7 @@ import json
 import os
 import random
 import hashlib
-import base64
+import datetime
 
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -24,7 +24,7 @@ if not os.path.exists(LAST_SAVED_FILE):
     with open(LAST_SAVED_FILE, 'w') as f:
         f.write('{}')
 
-def fetch_file(pkg: str = 'deb'):
+def fetch_file(pkg: str = 'deb') -> str | bool:
     """
     Fetches a file from a remote server based on the package type.
 
@@ -33,13 +33,35 @@ def fetch_file(pkg: str = 'deb'):
 
     Returns:
         str: Path to the downloaded file.
+        bool: False if no download was necessary.
     """
+
+    if pkg != 'deb':
+        raise NotImplementedError("Discord API only supports 'deb' packages on Linux at the moment.")
 
     url_req = URL + pkg
     response = requests.get(url_req)
     response.raise_for_status()
-    content = response.content
-    headers = response.headers
+
+    content = b""
+    headers = {}
+    request_md5 = ""
+    md5sum_content = ""
+
+    # Check request integrity
+    while True:
+        response = requests.get(url_req)
+        response.raise_for_status()
+        content = response.content
+        headers = response.headers
+        request_md5 = headers["etag"].replace('"', '')
+
+        md5sum_content = hashlib.md5(content).hexdigest()
+        if request_md5 == md5sum_content:
+            print("Request integrity verified.")
+            break
+
+        print("Request integrity failed. Retrying...")
 
     file_name = ""
     while True:
@@ -49,20 +71,54 @@ def fetch_file(pkg: str = 'deb'):
             break
 
     last_saved_json = json.load(open(LAST_SAVED_FILE, 'r'))
+    should_download = False
 
     if last_saved_json == {}:
+        should_download = True
+    
+    if not should_download:
+        if last_saved_json['md5'] != request_md5:
+            should_download = True
+
+        if pkg != last_saved_json['package_type']:
+            should_download = True
+
+        if not os.path.exists(os.path.join(DOWNLOAD_PATH, last_saved_json['filename'])):
+            should_download = True
+
+
+    if should_download:
         last_saved_json['filename'] = file_name
         last_saved_json['md5'] = headers["etag"].replace('"', '')
         last_saved_json["modified_time"] = headers["last-modified"]
+        last_saved_json["download_time"] = str(datetime.datetime.now())
+        last_saved_json["package_type"] = pkg
 
         with open(LAST_SAVED_FILE, 'w') as f:
             json.dump(last_saved_json, f, indent=4)
 
-    # Write file
-    with open(os.path.join(DOWNLOAD_PATH, file_name), 'wb') as f:
-        f.write(content)
+        # Write file and check write integrity
 
-    return os.path.join(DOWNLOAD_PATH, file_name)
+        while True:
+            download_path_file = os.path.join(DOWNLOAD_PATH, file_name)
+            with open(os.path.join(download_path_file), 'wb') as f:
+                f.write(content)
+
+            md5sum_file = hashlib.md5(open(download_path_file, 'rb').read()).hexdigest()
+            if md5sum_file == request_md5:
+                print("File write integrity verified.")
+                break
+            print("File write integrity failed. Retrying...")
+            
+
+        return os.path.join(DOWNLOAD_PATH, file_name)
+    
+    print("No download necessary.")
+    return False
+
+
+
+
 
 
 if __name__ == '__main__':
